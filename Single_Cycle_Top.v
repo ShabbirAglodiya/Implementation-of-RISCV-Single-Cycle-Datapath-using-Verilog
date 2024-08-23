94881d1a337b67a4f2398d9a0b0705ca1a03f288
ShabbirAglodiya <162858361+ShabbirAglodiya@users.noreply.github.com>
2024-08-23 10:14:33 +0530

    Add files via upload

---
+++ Single_Cycle_Top.v
@@ -0,0 +1,360 @@
+module Single_Cycle_Top(clk,rst);
+
+    input clk,rst;
+
+    wire [31:0] PC_Top,RD_Instr,RD1_Top,Imm_Ext_Top,ALUResult,ReadData,PCPlus4,RD2_Top,SrcB,Result;
+    wire RegWrite,MemWrite,ALUSrc,ResultSrc;
+    wire [1:0]ImmSrc;
+    wire [2:0]ALUControl_Top;
+
+    PC_Module PC(
+        .clk(clk),
+        .rst(rst),
+        .PC(PC_Top),
+        .PC_Next(PCPlus4)
+    );
+
+    PC_Adder PC_Adder(
+                    .a(PC_Top),
+                    .b(32'd4),
+                    .c(PCPlus4)
+    );
+    
+    Instruction_Memory Instruction_Memory(
+                            .rst(rst),
+                            .A(PC_Top),
+                            .RD(RD_Instr)
+    );
+
+    Register_File Register_File(
+                            .clk(clk),
+                            .rst(rst),
+                            .WE3(RegWrite),
+                            .WD3(Result),
+                            .A1(RD_Instr[19:15]),
+                            .A2(RD_Instr[24:20]),
+                            .A3(RD_Instr[11:7]),
+                            .RD1(RD1_Top),
+                            .RD2(RD2_Top)
+    );
+
+    Sign_Extend Sign_Extend(
+                        .In(RD_Instr),
+                        .ImmSrc(ImmSrc[0]),
+                        .Imm_Ext(Imm_Ext_Top)
+    );
+
+    Mux Mux_Register_to_ALU(
+                            .a(RD2_Top),
+                            .b(Imm_Ext_Top),
+                            .s(ALUSrc),
+                            .c(SrcB)
+    );
+
+    ALU ALU(
+            .A(RD1_Top),
+            .B(SrcB),
+            .Result(ALUResult),
+            .ALUControl(ALUControl_Top),
+            .OverFlow(),
+            .Carry(),
+            .Zero(),
+            .Negative()
+    );
+
+    Control_Unit_Top Control_Unit_Top(
+                            .Op(RD_Instr[6:0]),
+                            .RegWrite(RegWrite),
+                            .ImmSrc(ImmSrc),
+                            .ALUSrc(ALUSrc),
+                            .MemWrite(MemWrite),
+                            .ResultSrc(ResultSrc),
+                            .Branch(),
+                            .funct3(RD_Instr[14:12]),
+                            .funct7(RD_Instr[31:25]),
+                            .ALUControl(ALUControl_Top)
+    );
+
+    Data_Memory Data_Memory(
+                        .clk(clk),
+                        .rst(rst),
+                        .WE(MemWrite),
+                        .WD(RD2_Top),
+                        .A(ALUResult),
+                        .RD(ReadData)
+    );
+
+    Mux Mux_DataMemory_to_Register(
+                            .a(ALUResult),
+                            .b(ReadData),
+                            .s(ResultSrc),
+                            .c(Result)
+    );
+
+endmodule
+
+
+
+
+module Sign_Extend (In,Imm_Ext,ImmSrc);
+
+    input [31:0]In;
+    input ImmSrc;
+    output [31:0]Imm_Ext;
+
+    assign Imm_Ext = (ImmSrc == 1'b1) ? ({{20{In[31]}},In[31:25],In[11:7]}):
+                                        {{20{In[31]}},In[31:20]};
+                                
+endmodule
+
+
+module Register_File(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
+
+    input clk,rst,WE3;
+    input [4:0]A1,A2,A3;
+    input [31:0]WD3;
+    output [31:0]RD1,RD2;
+
+    reg [31:0] Register [31:0];
+
+    always @ (posedge clk)
+    begin
+        if(WE3)
+            Register[A3] <= WD3;
+    end
+
+    assign RD1 = (~rst) ? 32'd0 : Register[A1];
+    assign RD2 = (~rst) ? 32'd0 : Register[A2];
+
+    initial begin
+        Register[1] = 32'h00000001;
+        Register[2] = 32'h00000002;
+        Register[3] = 32'h00000003;
+        Register[4] = 32'h00000004;
+        Register[5] = 32'h00000005;
+        Register[6] = 32'h00000006;
+        Register[7] = 32'h00000007;
+        Register[8] = 32'h00000008;
+        Register[9] = 32'h00000009;        
+    end
+
+endmodule
+
+
+module PC_Adder (a,b,c);
+
+    input [31:0]a,b;
+    output [31:0]c;
+
+    assign c = a + b;
+    
+endmodule
+
+module PC_Module(clk,rst,PC,PC_Next);
+    input clk,rst;
+    input [31:0]PC_Next;
+    output [31:0]PC;
+    reg [31:0]PC;
+
+    always @(posedge clk)
+    begin
+        if(~rst)
+            PC <= {32{1'b0}};
+        else
+            PC <= PC_Next;
+    end
+endmodule
+
+
+module Mux (a,b,s,c);
+
+    input [31:0]a,b;
+    input s;
+    output [31:0]c;
+
+    assign c = (~s) ? a : b ;
+    
+endmodule
+
+
+
+module Main_Decoder(Op,RegWrite,ImmSrc,ALUSrc,MemWrite,ResultSrc,Branch,ALUOp);
+    input [6:0]Op;
+    output RegWrite,ALUSrc,MemWrite,ResultSrc,Branch;
+    output [1:0]ImmSrc,ALUOp;
+
+    assign RegWrite = (Op == 7'b0000011 | Op == 7'b0110011) ? 1'b1 :
+                                                              1'b0 ;
+    assign ImmSrc = (Op == 7'b0100011) ? 2'b01 : 
+                    (Op == 7'b1100011) ? 2'b10 :    
+                                         2'b00 ;
+    assign ALUSrc = (Op == 7'b0000011 | Op == 7'b0100011) ? 1'b1 :
+                                                            1'b0 ;
+    assign MemWrite = (Op == 7'b0100011) ? 1'b1 :
+                                           1'b0 ;
+    assign ResultSrc = (Op == 7'b0000011) ? 1'b1 :
+                                            1'b0 ;
+    assign Branch = (Op == 7'b1100011) ? 1'b1 :
+                                         1'b0 ;
+    assign ALUOp = (Op == 7'b0110011) ? 2'b10 :
+                   (Op == 7'b1100011) ? 2'b01 :
+                                        2'b00 ;
+
+endmodule
+
+
+
+module Instruction_Memory(rst,A,RD);
+
+  input rst;
+  input [31:0]A;
+  output [31:0]RD;
+
+  reg [31:0] mem [1023:0];
+  
+  assign RD = (~rst) ? {32{1'b0}} : mem[A[31:2]];
+
+  initial begin
+    $readmemh("memfile2.hex",mem);
+  end
+
+
+/*
+  initial begin
+    //mem[0] = 32'hFFC4A303;
+    //mem[1] = 32'h00832383;
+    // mem[0] = 32'h0064A423;
+    // mem[1] = 32'h00B62423;
+    mem[0] = 32'h0062E233;
+    // mem[1] = 32'h00B62423;
+
+  end
+*/
+endmodule
+
+
+
+module Data_Memory(clk,rst,WE,WD,A,RD);
+
+    input clk,rst,WE;
+    input [31:0]A,WD;
+    output [31:0]RD;
+
+    reg [31:0] mem [1023:0];
+
+    always @ (posedge clk)
+    begin
+        if(WE)
+            mem[A] <= WD;
+    end
+
+    assign RD = (~rst) ? 32'd0 : mem[A];
+
+    initial begin
+        mem[0] = 32'h00000000;
+        mem[1] = 32'h00000001;
+        mem[2] = 32'h00000002;
+        mem[3] = 32'h00000003;
+        mem[4] = 32'h00000004;
+        mem[5] = 32'h00000005;
+        mem[6] = 32'h00000006;
+        mem[7] = 32'h00000007;
+        mem[8] = 32'h00000008;
+        mem[9] = 32'h00000009;
+        
+    end
+
+
+endmodule
+
+
+
+module Control_Unit_Top(Op,RegWrite,ImmSrc,ALUSrc,MemWrite,ResultSrc,Branch,funct3,funct7,ALUControl);
+
+    input [6:0]Op,funct7;
+    input [2:0]funct3;
+    output RegWrite,ALUSrc,MemWrite,ResultSrc,Branch;
+    output [1:0]ImmSrc;
+    output [2:0]ALUControl;
+
+    wire [1:0]ALUOp;
+
+    Main_Decoder Main_Decoder(
+                .Op(Op),
+                .RegWrite(RegWrite),
+                .ImmSrc(ImmSrc),
+                .MemWrite(MemWrite),
+                .ResultSrc(ResultSrc),
+                .Branch(Branch),
+                .ALUSrc(ALUSrc),
+                .ALUOp(ALUOp)
+    );
+
+    ALU_Decoder ALU_Decoder(
+                            .ALUOp(ALUOp),
+                            .funct3(funct3),
+                            .funct7(funct7),
+                            .op(Op),
+                            .ALUControl(ALUControl)
+    );
+
+
+endmodule
+
+
+
+module ALU_Decoder(ALUOp,funct3,funct7,op,ALUControl);
+
+    input [1:0]ALUOp;
+    input [2:0]funct3;
+    input [6:0]funct7,op;
+    output [2:0]ALUControl;
+
+    // Method 1 
+    // assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
+    //                     (ALUOp == 2'b01) ? 3'b001 :
+    //                     (ALUOp == 2'b10) ? ((funct3 == 3'b000) ? ((({op[5],funct7[5]} == 2'b00) | ({op[5],funct7[5]} == 2'b01) | ({op[5],funct7[5]} == 2'b10)) ? 3'b000 : 3'b001) : 
+    //                                         (funct3 == 3'b010) ? 3'b101 : 
+    //                                         (funct3 == 3'b110) ? 3'b011 : 
+    //                                         (funct3 == 3'b111) ? 3'b010 : 3'b000) :
+    //                                        3'b000;
+
+    // Method 2
+    assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
+                        (ALUOp == 2'b01) ? 3'b001 :
+                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} == 2'b11)) ? 3'b001 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} != 2'b11)) ? 3'b000 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b010)) ? 3'b101 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b110)) ? 3'b011 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b111)) ? 3'b010 : 
+                                                                  3'b000 ;
+endmodule
+
+
+
+module ALU(A,B,Result,ALUControl,OverFlow,Carry,Zero,Negative);
+
+    input [31:0]A,B;
+    input [2:0]ALUControl;
+    output Carry,OverFlow,Zero,Negative;
+    output [31:0]Result;
+
+    wire Cout;
+    wire [31:0]Sum;
+
+    assign {Cout,Sum} = (ALUControl[0] == 1'b0) ? A + B :
+                                          (A + ((~B)+1)) ;
+    assign Result = (ALUControl == 3'b000) ? Sum :
+                    (ALUControl == 3'b001) ? Sum :
+                    (ALUControl == 3'b010) ? A & B :
+                    (ALUControl == 3'b011) ? A | B :
+                    (ALUControl == 3'b101) ? {{31{1'b0}},(Sum[31])} : {32{1'b0}};
+    
+    assign OverFlow = ((Sum[31] ^ A[31]) & 
+                      (~(ALUControl[0] ^ B[31] ^ A[31])) &
+                      (~ALUControl[1]));
+    assign Carry = ((~ALUControl[1]) & Cout);
+    assign Zero = &(~Result);
+    assign Negative = Result[31];
+
+endmodule
